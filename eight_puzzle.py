879df716c00959b236074638a5d55da0968ad6a5
Aidan Ching <achin053@ucr.edu>
2023-02-08 21:29:25 -0800

    Completed project #1

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -1,4 +1,5 @@
 import random
+import math
 from copy import deepcopy
 
 
@@ -6,32 +7,73 @@ EMPTY = None
 
 
 class Node:
-    def __init__(self, board = None, parent = None):
+    def __init__(self, board = None, parent = None, depth = 0, heuristic = 0):
         self.board = board
         self.parent = parent
-        self.value = utility(board)
+        self.depth = depth
+        if heuristic == 0:
+            self.value = heuristic_correct_tile(board)
+        self.misplaced_tiles = misplaced_tiles(board)
+        self.manhattan_distance = manhattan_distance(board)
         
 
-def initial_state():
+def initial_state(size):
     #returns the starting state of the board
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
+    nums_in_board = list(range(1, size**2))
+    nums_in_board.append(0)
 
-def shuffle(board):
-    count = len(board) * len(board[0])
-    temp_arr = list(range(0, count))
+    board = list()
+    for i in range(size):
+        temp = list()
+        for j in range(size):
+            temp.append(nums_in_board[i*size + j])
+        board.append(temp)
+    return board
 
-    random.shuffle(temp_arr)
+#returns a board with paramaters size and how many moves that it needs to be solved.
+def random_board(size, moves):
 
-    for i, row in enumerate(board):
-        for j, tile in enumerate(row):
-            row[j] = temp_arr[i*len(board[0]) + j]
+    board = initial_state(size)
+    explored = list()
+    for i in range(moves):
+        explored.append(board)
+        all_possible_moves = actions(board)
+        possible = list()
+        for action in all_possible_moves:
+            if action not in explored:
+                possible.append(action)
+        board = random.choice(possible)
+    return board
+
+def manhattan_distance(board):
+    count = 0
+    for y, row in enumerate(board):
+        for x, num in enumerate(row):
+            if num == 1:
+                count += abs(x-0)+abs(y-0)
+            if num == 2:
+                count += abs(x-1)+abs(y-0)
+            if num == 3:
+                count += abs(x-2)+abs(y-0)
+            if num == 4:
+                count += abs(x-0)+abs(y-1)
+            if num == 5:
+                count += abs(x-1)+abs(y-1)
+            if num == 6:
+                count += abs(x-2)+abs(y-1)
+            if num == 7:
+                count += abs(x-0)+abs(y-2)
+            if num == 8:
+                count += abs(x-1)+abs(y-2)
+
+    return count
+
+        
+
 
 def display(board):
     for row in board:
         print(row)
-    print("\n")
 
 def goal(board):
     temp_arr = list()
@@ -85,7 +127,7 @@ def actions(board):
 
     return moves
 
-def utility(board):
+def heuristic_correct_tile(board):
     #returns how many of the numbers are in the correct place
     count = 0
     for i, row in enumerate(board):
@@ -95,38 +137,158 @@ def utility(board):
     return count
 
 def uniform_cost_search(start):
+    print("Starting State:")
+    display(start.board)
+
     p_queue = list()
     p_queue.append(start)
 
     explored = list()
+
+    num_expanded_nodes = 0
+    max_nodes_in_queue = 0
 
     while p_queue:
         #take node that has the most value
         max_val = 0
         currNode = None
         #find max val
-        for node in p_queue:
-            if node.value > max_val:
-                max_val = node.value
-        #pop first node with the max val
-        for i, node in enumerate(p_queue):
-            if node.value == max_val:
-                currNode = p_queue.pop(i)
-                break
+        p_queue = sorted(p_queue, key=lambda node: node.depth)
+        max_nodes_in_queue = max(max_nodes_in_queue, len(p_queue))
+        
+
+        currNode = p_queue.pop(0)
+        print("The best state to expand with g(n) = " + str(currNode.depth) + " is...")
+        display(currNode.board)
+        print("Expanding this node...\n")
+        num_expanded_nodes += 1
+
+        if goal(currNode.board):
+            print("Goal!!!\n")
+            print("To solve this problem the search algorithm expanded a total of " + str(num_expanded_nodes) + " nodes.")
+            print("The maximum number of nodes in the queue at any one time: " + str(max_nodes_in_queue))
+            print("The depth of the goal node was " + str(currNode.depth))            
+            return currNode
         #get all the possible moves, add to explored nodes
         possible_moves = actions(currNode.board)
         explored.append(currNode.board)
         #if goal, return, else add to the priority queue
         for move in possible_moves:
-            if goal(move):
-                return Node(move, currNode)
             if move not in explored:
-                p_queue.append(Node(move, currNode))
+                p_queue.append(Node(move, currNode, currNode.depth+1, 0))
 
     return None
 
+def A_star_misplaced_tile(start):
+    print("Starting State:")
+    display(start.board)
+
+    p_queue = list()
+    p_queue.append(start)
+
+    explored = list()
+
+    num_expanded_nodes = 0
+    max_nodes_in_queue = 0
+
+    while p_queue:
+        #take node that has the most value
+        max_val = 0
+        currNode = None
+        #find max val
+        p_queue = sorted(p_queue, key=lambda node: node.depth+node.misplaced_tiles)
+        max_nodes_in_queue = max(max_nodes_in_queue, len(p_queue))
+        
+
+        currNode = p_queue.pop(0)
+        print("The best state to expand with g(n) = " + str(currNode.depth) + " and h(n) = " + str(currNode.misplaced_tiles) + " with a total of: " + str(currNode.depth+currNode.misplaced_tiles) + " is...")
+        display(currNode.board)
+        print("Expanding this node...\n")
+        num_expanded_nodes += 1
+
+        if goal(currNode.board):
+            print("Goal!!!\n")
+            print("To solve this problem the search algorithm expanded a total of " + str(num_expanded_nodes) + " nodes.")
+            print("The maximum number of nodes in the queue at any one time: " + str(max_nodes_in_queue))
+            print("The depth of the goal node was " + str(currNode.depth))        
+            return currNode
+        #get all the possible moves, add to explored nodes
+        possible_moves = actions(currNode.board)
+        explored.append(currNode.board)
+        #if goal, return, else add to the priority queue
+        for move in possible_moves:
+            if move not in explored:
+                p_queue.append(Node(move, currNode, currNode.depth+1, 0))
+
+    return None
+
+def string_to_board(inputstr):
+    board = initial_state(int(math.sqrt(len(inputstr))))
+    temp_arr = list()
+    for char in inputstr:
+        temp_arr.append(int(char))
+
+    for i, row in enumerate(board):
+        for j, tile in enumerate(row):
+            row[j] = temp_arr[i*len(board[0]) + j]
+
+    return board
+
+
+def A_star_manhattan_distance(start):
+    print("Starting State:")
+    display(start.board)
+
+    p_queue = list()
+    p_queue.append(start)
+
+    explored = list()
+
+    num_expanded_nodes = 0
+    max_nodes_in_queue = 0
+
+    while p_queue:
+        #take node that has the most value
+        max_val = 0
+        currNode = None
+        #find max val
+        p_queue = sorted(p_queue, key=lambda node: node.depth+node.manhattan_distance)
+        max_nodes_in_queue = max(max_nodes_in_queue, len(p_queue))
+        
+
+        currNode = p_queue.pop(0)
+        print("The best state to expand with g(n) = " + str(currNode.depth) + " and h(n) = " + str(currNode.manhattan_distance) + " and f(n) = " + str(currNode.depth+currNode.manhattan_distance) + " is...")
+        display(currNode.board)
+        print("Expanding this node...\n")
+        num_expanded_nodes += 1
+
+        if goal(currNode.board):
+            print("Goal!!!\n")
+            print("To solve this problem the search algorithm expanded a total of " + str(num_expanded_nodes) + " nodes.")
+            print("The maximum number of nodes in the queue at any one time: " + str(max_nodes_in_queue))
+            print("The depth of the goal node was " + str(currNode.depth))          
+            return currNode
+        #get all the possible moves, add to explored nodes
+        possible_moves = actions(currNode.board)
+        explored.append(currNode.board)
+        #if goal, return, else add to the priority queue
+        for move in possible_moves:
+            if move not in explored:
+                p_queue.append(Node(move, currNode, currNode.depth+1, 0))
+
+    return None
+
+def misplaced_tiles(board):
+    count = 0
+    for i, row in enumerate(board):
+        for j, num in enumerate(row):
+            if num != i*len(board) + j + 1 and i*len(board)+j+1 != len(board)**2:
+                count += 1
+    return count
 
 def displayTrace(solution):
+    depth = solution.depth
+
     trace = list()
     while solution.parent != None:
         trace.append(solution.board)
@@ -135,6 +297,7 @@ def displayTrace(solution):
 
     for move in trace:
         display(move)
+    print("Solved in " + str(depth) + " steps.")
 
         
 
--- runner.py
+++ runner.py
@@ -1,19 +1,36 @@
 import eight_puzzle as eight
+import time
 from eight_puzzle import Node
 
-board = eight.initial_state() 
+print("Welcome to 862210672 8 puzzle solver.")
+puzzle_choice = input("Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle, or \"3\" to generate a random puzzle\n")
+if puzzle_choice == "1":
+    inputstr = "185762340"
+    board = eight.string_to_board(inputstr)
+elif puzzle_choice == "2":
+    inputstr = input("Enter a 9 digit number representing the 8 puzzle.\n")
+    board = eight.string_to_board(inputstr)
+elif puzzle_choice == "3":
+    shuffle_amt = input("Enter how many moves to shuffle the puzzle from the original position: ")
+    board = eight.random_board(3, int(shuffle_amt))
 
+print("Enter your choice of algorithm: ")
+print("1. Uniform Cost Search")
+print("2. A* with the Misplaced Tile heuristic")
+print("3. A* with the Euclidean distance heuristic\n")
 
-#our board for testing purposes
-temp_arr = [2,8,4,1,5,0,6,7,3]
-
-for i, row in enumerate(board):
-    for j, tile in enumerate(row):
-        row[j] = temp_arr[i*len(board[0]) + j]
-
-
+algo_choice = input()
 start = Node(board, None)
 
-solution = eight.uniform_cost_search(start)
-
-eight.displayTrace(solution)
+if algo_choice == "1":
+    start_time = time.time()
+    solution = eight.uniform_cost_search(start)
+    print("--- %s seconds ---" % (time.time() - start_time))
+elif algo_choice == "2":
+    start_time = time.time()
+    solution = eight.A_star_misplaced_tile(start)
+    print("--- %s seconds ---" % (time.time() - start_time))
+elif algo_choice == "3":
+    start_time = time.time()
+    solution = eight.A_star_manhattan_distance(start)
+    print("--- %s seconds ---" % (time.time() - start_time))
